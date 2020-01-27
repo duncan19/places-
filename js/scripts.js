a4fe4ef0c6e1a1d317468cce6674f18352dfd9d7
Duncan Robbins and Jonathan Carlos <jonathan@>
2020-01-27 14:40:11 -0800

    allow program to hide and display details of locations

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,23 @@ PlacesList.prototype.assignId = function() {
   return this.currentId;
 }
 
+PlacesList.prototype.findPlace = function(id) {
+  for (var i=0; i< this.places.length; i++) {
+    if (this.places[i]) {
+      if (this.places[i].id == id) {
+        return this.places[i];
+      }
+    }
+  };
+  return false;
+}
 
-function Place(location, landmark, timeOfYear){
+
+function Place(location, landmark, timeOfYear) {
   this.location = location;
   this.landmark = landmark; 
   this.timeOfYear = timeOfYear;
   }
-
-
-
 
   var placeList = new PlacesList();
 
@@ -27,21 +35,39 @@ function Place(location, landmark, timeOfYear){
     var detailsList = $("ul#places");
     var htmlForLocationInfo = "";
     placesListToDisplay.places.forEach(function(place) {
-      htmlForLocationInfo += "<li id=" + place.id + ">" + place.location + " " + place.landmark + " " + place.timeOfYear + "</li>";
+      htmlForLocationInfo += "<li id=" + place.id + ">" + place.location + "</li>";
     });
+    // placesListToDisplay.places.forEach(function(place) {
+    //   htmlForLocationInfo += "<li id=" + place.id + ">" + place.location + " " + place.landmark + " " + place.timeOfYear + "</li>";
+    // });
     detailsList.html(htmlForLocationInfo);
   }
+
+  function showPlaces(placeID) {
+    var place = placeList.findPlace(placeID);
+  
+    
+    $("#show-place").show();
+    $(".location").html(place.location);
+    $(".landmark").html(place.landmark);
+    $(".timeOfYear").html(place.timeOfYear);
+  }
+
+  function attachContactListeners() {
+    $("ul#places").on("click", "li", function() {
+      showPlaces(this.id)
+    });
+  }
+
 $(document).ready(function() {
+  attachContactListeners();    
   $("#user-places").submit(function(event) {
     event.preventDefault();
     var location = $("input#location").val();
     var landmark = $("input#landmark").val();
     var timeOfYear = $("input#time-of-year").val();
-    var newPlace = new Place(location, landmark, timeOfYear)
+    var newPlace = new Place(location, landmark, timeOfYear);
     placeList.addPlace(newPlace);
     displayPlacesDetails(placeList);
-  
-  
-    
-  })
+  });
 })
